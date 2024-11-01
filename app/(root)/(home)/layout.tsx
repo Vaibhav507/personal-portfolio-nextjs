2fc8ce0a63c6c017ba83446c32091d4068ba2e7b
unknown <vaibhavpratham507@gmail.com>
2024-11-01 18:12:10 +0530

    Cursor Animation

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -2,10 +2,22 @@
 
 import React, { ReactNode, useState } from 'react';
 import Navbar from '@/components/Navbar';
+import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
 
 
 
 const HomeLayout = ({children}:{children : ReactNode}) => {
+
+  useGSAP(()=>{
+    window.addEventListener("mousemove", function(dets) {
+      gsap.to("#cursor", {
+          x: dets.x,
+          y: dets.y,
+          duration: 1,
+      })
+    })
+  })
 
   const [menuOpen, setmenuOpen] = useState(false);
   const toggleMenu = () => {
@@ -14,6 +26,7 @@ const HomeLayout = ({children}:{children : ReactNode}) => {
 
   return (
     <main className='relative overflow-x-hidden'>
+      <div className="h-5 w-5 bg-white rounded-full fixed z-[100] mix-blend-difference" id='cursor'></div>
         <Navbar />
         <div className="flex">
             <section className="flex flex-1 min-h-screen px-8 sm:px-28 md:px-48 justify-start items-center">
